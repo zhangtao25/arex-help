91a92870ae9cc6042548dfa028073d208df0af7e
zhangtao25 <tzhangm@trip.com>
2022-07-18 15:06:23 +0800

    upd

--- .next/static/8LwicKc2JjQttjZ0PwGyu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/987-6553ca1d202dd603.js","static/chunks/pages/index-472e3fee46b491e3.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
