1c38c9da0fbf68622bf45ebdf3f3999e60cdf256
zhangtao25 <tzhangm@trip.com>
2022-07-18 15:04:59 +0800

    upd

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.js',
+    // unstable_staticImage: true,
+    // images: {
+    //     unoptimized:true
+    // },
+    // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+module.exports = withNextra()
